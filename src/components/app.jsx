e3d065aac507e940d8546d37f58d4b346d5cbf9c
James Wordie <james.wordie@yahoo.com>
2021-06-24 11:50:58 +0100

    created react app and done initial setup

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './search-bar';
+
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: '20px' }}>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
