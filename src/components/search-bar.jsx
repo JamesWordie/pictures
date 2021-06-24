b7590d1e88b1358405199a212a8f33faf6d8f563
James Wordie <james.wordie@yahoo.com>
2021-06-24 12:21:14 +0100

    changes for the controlled component

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // onInputChange(event) {
+  //   // console.log(event.target.value);
+  //   const search = event.target.value;
+  // }
+  state = { term: "" }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label htmlFor="image">Image Search</label>
-            <input className="prompt" type="text" name="image" />
-{/*            <i class="search icon"></i>
-*/}          </div>
+            <input
+              type="text"
+              value={this.state.term}
+              id="image"
+              onChange={(event) => this.setState({ term: event.target.value })}
+            />
+          </div>
         </form>
       </div>
     );
